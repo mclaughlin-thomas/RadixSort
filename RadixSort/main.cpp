8dfb3618cff40a2fa438916d7a21d74ff6ef8bf3
mclaughlin-thomas <thomas.mclaughlin2@stvincent.edu>
2024-09-27 22:36:52 -0400

    Updated documentation all

--- RadixSort/main.cpp
+++ RadixSort/main.cpp
@@ -3,13 +3,7 @@
 // CS-312: Introduction to Algorithms
 
 /*
-Summary:  Follow our textbook's design of a RADIX-SORT that uses COUNTING-SORT internally.
-Use it to sort an array of 4-digit decimal numbers. The array should contain 100 4-digit numbers in
-apparent random order at the start. The output should show on the screen the sorted data, perhaps with 10
-numbers per line. Follow the pseudocode in our textbook closely. Be careful with lines 12 and 13 especially.
-Don't forget the documentation: A summary of what the program does overall at the top of the file containing
-main, and documentation for each function other than main, where this documentation is either in the
-precondition/postcondition style or given/task/return style.
+Summary:  x
 
 
 Programming Assignment:  Follow our textbook's design of a RADIX-SORT that uses COUNTING-SORT internally.
@@ -28,59 +22,59 @@ precondition/postcondition style or given/task/return style.
 #include <ctime>
 
 
-// CHANGE TO DESIRE ---
-constexpr int MAX_DIGITS = 4;   // Number of places in the form: xxxx
-constexpr int ARRAY_SIZE = 100; // Size of the array
+// CHANGE TO DESIRE BELOW ---
+constexpr int MAX_DIGITS = 4;   // Number of places in the form: xxxx.
+constexpr int ARRAY_SIZE = 100; // Size of the array.
 constexpr int RANGE = 9999;     // 9999 to adhere to 4 place digits, 99999 to adhere to 5 place digits, and so on.
-// CHANGE TO DESIRE ---
-const int MAX_PLACE = static_cast<int>(pow(10, (MAX_DIGITS - 1)));  // Smallest power of 10 that has the number of digits specified by MAX_DIGITS
+// CHANGE TO DESIRE ABOVE ---
+const int MAX_PLACE = static_cast<int>(pow(10, (MAX_DIGITS - 1)));  // Smallest power of 10 that has the number of digits specified by MAX_DIGITS.
 
 
-// Given:  numbers      - An array
-//         B            - An array
-//         digitPlace   - The maximum number of desired digits for each integer in numbers array.
+// Given:  Numbers      - The array wished to be sorted.
+//         B            - A temporary array to store the sorted numbers based upon the current digit place.
+//         digitPlace   - The place we wish to evaluate: unit place, tens place, hundreds place, etc.
 // 
-// Task:   XXXXX
+// Task:   To sort the Numbers array into ascending order based on the digit indicated by the parameter digitPlace.
 // 
-// Return: XXXXX        - XXXXX
-void CountingSort(int numbers[], int B[], const int digitPlace);
+// Return: B            - Containing the data from numbers which is sorted into ascending order by the digitPlace.
+void CountingSort(int Numbers[], int B[], const int digitPlace);
 
 
-// Given:  numbers      - An array of unsorted integers.
-//         maxPlace     - The size of the array.
+// Given:  Numbers      - An array of unsorted integers.
+//         maxPlace     - Integer which is the smallest power of 10 that has the number of digits specified by MAX_DIGITS.
 // 
-// Task:   To sort the integers in A into ascending order by using radix sort (with CountingSort as its stable sort).
+// Task:   To sort the integers in the Numbers array into ascending order using Radix Sort (with CountingSort as its stable sort).
 // 
-// Return: numbers      - An array of integers, now in sorted, ascending order.
-void RadixSort(int numbers[], const int maxPlace);
+// Return: Numbers      - An array of integers, now in sorted, ascending order.
+void RadixSort(int Numbers[], const int maxPlace);
 
 
-// Given:  numbers      - An array to be populated with random integers.
+// Given:  Numbers      - An array to be populated with random integers.
 //         size         - The size of the array.
 //         maxDigits    - The maximum number of desired digits for each integer in numbers array.
 // 
-// Task:   Populate the given array with random integers, where each integer has a maximum of
+// Task:   Populate the Numbers array with random integers, where each integer has a maximum of
 //         maxDigits places. The random integers are generated in the range from 10^(n-1) to the largest
 //         number that can be represented without exceeding the maximum number of places: maxDigits.
 // 
-// Return: numbers      - An array of integers populated with random integers with the desired number of places.
-void populateArray(int numbers[], const int size ,const int maxDigits);
+// Return: Numbers      - An array of integers populated with random integers with the desired number of places.
+void populateArray(int Numbers[], const int size ,const int maxDigits);
 
 
 
 int main(void) {
 
-    std::srand((unsigned)time(0)); // Seeding with time to guarantee different set of numbers each run
+    std::srand((unsigned)time(0)); // Seeding with time to guarantee different set of numbers each run.
 
-    int numbers[ARRAY_SIZE];
-    populateArray(numbers, ARRAY_SIZE, MAX_DIGITS); // Populate the numbers array with random numbers in specified range
+    int Numbers[ARRAY_SIZE];
+    populateArray(Numbers, ARRAY_SIZE, MAX_DIGITS); // Populate the Numbers array with random numbers in specified range.
 
-    RadixSort(numbers, MAX_PLACE); // Sort the numbers array in ascending order
+    RadixSort(Numbers, MAX_PLACE); // Sort the Numbers array in ascending order.
 
-    // Output the newly sorted array
+    // Output the newly sorted array.
     std::cout << "SORTED ARRAY" << std::endl;
     for (int i = 0; i < ARRAY_SIZE; i++) {
-        std::cout <<  numbers[i] << " ";
+        std::cout << Numbers[i] << " ";
 
         if ((i + 1) % 10 == 0) {
             std::cout << std::endl;
@@ -92,11 +86,11 @@ int main(void) {
 
 
 
-void CountingSort(int numbers[], int B[], const int digitPlace) {
+void CountingSort(int Numbers[], int B[], const int digitPlace) {
 
     // Counting Sort has a worst case time of theta ( k + n )
     // Counting Sort has an average case time of theta ( k + n )
-    // Where k is the # of values
+    // Where k is the # of values.
 
     //      COUNTING-SORT(A, n, k)
     // 1         let B[1:n] and C[0:k] be new arrays   | I instead initialized all of C's elements to 0 w/o a loop, and passed in array B.
@@ -114,12 +108,12 @@ void CountingSort(int numbers[], int B[], const int digitPlace) {
     // 13            C[A[j]] = C[A[j]] - 1
     // 14        return B                              | B[] was passed in by reference as a parameter, we 'return' B this way.
 
-    int C[RANGE] = { 0 }; // RANGE acts as K: if max number goes up to 7, the range is 8 (0-7)
+    int C[RANGE] = { 0 }; // RANGE acts as K: if max number goes up to 7, the range is 8 (0-7).
     int digit;
 
     // Count the occurrences.
     for (int j = 0; j < ARRAY_SIZE; j++) {
-        digit = (numbers[j] / digitPlace) % RANGE; // Get the digit at the current place value.
+        digit = (Numbers[j] / digitPlace) % RANGE; // Get the digit at the current place value.
         C[digit] = C[digit] + 1;
         // C[i] now contains the number of elements equal to i.
     }
@@ -129,21 +123,21 @@ void CountingSort(int numbers[], int B[], const int digitPlace) {
         C[i] = C[i] + C[i - 1];
     }
 
-    // Copy numbers to B, starting from the end of numbers.
+    // Copy numbers to array B, starting from the end of the Numbers array.
     for (int j = ARRAY_SIZE - 1; j >= 0; j--) {
-        digit = (numbers[j] / digitPlace) % RANGE; // Get the digit at the current place value.
-        B[C[digit] - 1] = numbers[j]; // Put the element in its proper location in B based on the digit
+        digit = (Numbers[j] / digitPlace) % RANGE; // Get the digit at the current place value.
+        B[C[digit] - 1] = Numbers[j]; // Put the element in its proper location in B based on the digit.
         C[digit] = C[digit] - 1; // Decrement the count so we can handle duplicates.
     }
 
 }
 
 
-void RadixSort(int numbers[], const int maxPlace) {
+void RadixSort(int Numbers[], const int maxPlace) {
 
     // Radix Sort has a worst case time of theta ( d (n + k) )
     // Radix Sort has an average case time of theta ( d (n + k) )
-    // Where k is the # of values, d is the # of digits, and n is the length of the array
+    // Where k is the # of values, d is the # of digits, and n is the length of the array.
 
     //      RADIX-SORT(A, n, d)
     // 1         for i = 1 to d
@@ -154,11 +148,11 @@ void RadixSort(int numbers[], const int maxPlace) {
     // Iterate over each digit place
     for (int digitPlace = 1; digitPlace <= maxPlace; digitPlace *= 10) {
 
-        CountingSort(numbers, B, digitPlace); // CountingSort will sort based on the provided digit place
+        CountingSort(Numbers, B, digitPlace); // CountingSort will sort based on the provided digit place.
 
-        // Now copying B to randomNumbers. Note that this wastes some time, O(n) time, where n is the length of the array
+        // Now copying B to Numbers array. Note that this wastes some time, O(n) time, where n is the length of the array.
         for (int k = 0; k < ARRAY_SIZE; k++) {
-            numbers[k] = B[k];
+            Numbers[k] = B[k];
         }
 
     }
@@ -166,14 +160,14 @@ void RadixSort(int numbers[], const int maxPlace) {
 }
 
 
-void populateArray(int numbers[], const int size, const int maxDigits) {
+void populateArray(int Numbers[], const int size, const int maxDigits) {
 
-    const int maxPlace = static_cast<int>(pow(10, maxDigits)) - 1; // With current MAX_DIGITS, the maximum random value is 9999
-    const int minPlace = static_cast<int>(pow(10, maxDigits-1)); // With current MAX_DIGITS, the maximum random value is 1000
+    const int maxPlace = static_cast<int>(pow(10, maxDigits)) - 1; // With current MAX_DIGITS, the maximum random value is 9999.
+    const int minPlace = static_cast<int>(pow(10, maxDigits-1)); // With current MAX_DIGITS, the maximum random value is 1000.
 
     // Generate random maxDigits digit numbers
     for (int i = 0; i < size; i++) {
-        numbers[i] = std::rand() % ( maxPlace - minPlace + 1 ) + minPlace; // Provides numbers from minPlace to 1 below next 10^n: (10^(n) -1 ).Thus resulting in numbers that have no more than our desired places of digits
+        Numbers[i] = std::rand() % ( maxPlace - minPlace + 1 ) + minPlace; // Provides numbers from minPlace to 1 below next 10^n: (10^(n) -1 ).Thus resulting in numbers that have no more than our desired places of digits.
     }
 
 }
