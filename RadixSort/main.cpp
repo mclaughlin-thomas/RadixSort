6ed6099c2332e24ed4f9808bad3cf72927454e66
mclaughlin-thomas <thomas.mclaughlin2@stvincent.edu>
2024-09-27 22:03:24 -0400

    Updated documentation CountingSort

--- RadixSort/main.cpp
+++ RadixSort/main.cpp
@@ -3,6 +3,15 @@
 // CS-312: Introduction to Algorithms
 
 /*
+Summary:  Follow our textbook's design of a RADIX-SORT that uses COUNTING-SORT internally.
+Use it to sort an array of 4-digit decimal numbers. The array should contain 100 4-digit numbers in
+apparent random order at the start. The output should show on the screen the sorted data, perhaps with 10
+numbers per line. Follow the pseudocode in our textbook closely. Be careful with lines 12 and 13 especially.
+Don't forget the documentation: A summary of what the program does overall at the top of the file containing
+main, and documentation for each function other than main, where this documentation is either in the
+precondition/postcondition style or given/task/return style.
+
+
 Programming Assignment:  Follow our textbook's design of a RADIX-SORT that uses COUNTING-SORT internally.
 Use it to sort an array of 4-digit decimal numbers. The array should contain 100 4-digit numbers in
 apparent random order at the start. The output should show on the screen the sorted data, perhaps with 10
@@ -20,9 +29,9 @@ precondition/postcondition style or given/task/return style.
 
 
 // CHANGE TO DESIRE ---
-constexpr int MAX_DIGITS = 4;   // Numbers in the form: xxxx
+constexpr int MAX_DIGITS = 4;   // Number of places in the form: xxxx
 constexpr int ARRAY_SIZE = 100; // Size of the array
-constexpr int RANGE = 9999;     // 9999 to adhere to 4 digit, 99999 to adhere to 5 digit, and so on.
+constexpr int RANGE = 9999;     // 9999 to adhere to 4 place digits, 99999 to adhere to 5 place digits, and so on.
 // CHANGE TO DESIRE ---
 const int MAX_PLACE = static_cast<int>(pow(10, (MAX_DIGITS - 1)));  // Smallest power of 10 that has the number of digits specified by MAX_DIGITS
 
@@ -36,13 +45,15 @@ const int MAX_PLACE = static_cast<int>(pow(10, (MAX_DIGITS - 1)));  // Smallest 
 // Return: XXXXX        - XXXXX
 void CountingSort(int numbers[], int B[], const int digitPlace);
 
-// Given:  numbers      - An array
+
+// Given:  numbers      - An array of unsorted integers.
 //         maxPlace     - The size of the array.
 // 
-// Task:   XXXXX
+// Task:   To sort the integers in A into ascending order by using radix sort (with CountingSort as its stable sort).
 // 
 // Return: numbers      - An array of integers, now in sorted, ascending order.
 void RadixSort(int numbers[], const int maxPlace);
+
 
 // Given:  numbers      - An array to be populated with random integers.
 //         size         - The size of the array.
@@ -54,6 +65,7 @@ void RadixSort(int numbers[], const int maxPlace);
 // 
 // Return: numbers      - An array of integers populated with random integers with the desired number of places.
 void populateArray(int numbers[], const int size ,const int maxDigits);
+
 
 
 int main(void) {
@@ -79,37 +91,53 @@ int main(void) {
 }
 
 
+
 void CountingSort(int numbers[], int B[], const int digitPlace) {
 
     // Counting Sort has a worst case time of theta ( k + n )
     // Counting Sort has an average case time of theta ( k + n )
     // Where k is the # of values
 
+    //      COUNTING-SORT(A, n, k)
+    //1         let B[1:n] and C[0:k] be new arrays   | I instead initialized all of C's elements to 0 w/o a loop, and passed in array B.
+    //2         for i = 0 to k
+    //3             C[i] = 0                          | Concatenated line with: int C[RANGE] = { 0 };
+    //4         for j = 1 to n
+    //5             C[A[j]] = C[A[j]] + 1
+    //6         // C[i] now contains the number of elements equal to i.
+    //7         for i = 1 to k
+    //8             C[i] = C[i] + C[i - 1]
+    //9         // C[i] now contains the number of elements less than or equal to i.
+    //10        // Copy A to B, starting from the end of A.
+    //11        for j = n down to 1
+    //12            B[C[A[j]]] = A[j]
+    //13            C[A[j]] = C[A[j]] - 1
+    //14        return B                              | B[] was passed in by reference as a parameter, we 'return' B this way.
+
     int C[RANGE] = { 0 }; // RANGE acts as K: if max number goes up to 7, the range is 8 (0-7)
     int digit;
 
-    //count occurrences
+    // Count the occurrences.
     for (int j = 0; j < ARRAY_SIZE; j++) {
-        digit = (numbers[j] / digitPlace) % RANGE; // Get the digit at the current place value
+        digit = (numbers[j] / digitPlace) % RANGE; // Get the digit at the current place value.
         C[digit] = C[digit] + 1;
-        //C[randomNumbers[j]] = C[randomNumbers[j]] + 1; //OLD VERSION
         // C[i] now contains the number of elements equal to i.
     }
 
-    //sum up
+    // Sum up elements across array.
     for (int i = 1; i < RANGE; i++) {
         C[i] = C[i] + C[i - 1];
     }
 
+    // Copy numbers to B, starting from the end of numbers.
     for (int j = ARRAY_SIZE - 1; j >= 0; j--) {
-        digit = (numbers[j] / digitPlace) % RANGE; // Get the digit at the current place value
-        B[C[digit] - 1] = numbers[j];
-        C[digit] = C[digit] - 1;
-        //B[C[randomNumbers[j]] - 1] = randomNumbers[j]; // using the -1 because i'm starting from 0
-        //C[randomNumbers[j]] = C[randomNumbers[j]] - 1;
+        digit = (numbers[j] / digitPlace) % RANGE; // Get the digit at the current place value.
+        B[C[digit] - 1] = numbers[j]; // Put the element in its proper location in B based on the digit
+        C[digit] = C[digit] - 1; // Decrement the count so we can handle duplicates.
     }
 
 }
+
 
 void RadixSort(int numbers[], const int maxPlace) {
 
@@ -132,6 +160,7 @@ void RadixSort(int numbers[], const int maxPlace) {
     }
 
 }
+
 
 void populateArray(int numbers[], const int size, const int maxDigits) {
 
